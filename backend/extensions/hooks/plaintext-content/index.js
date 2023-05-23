71339fb71b5c81c5df56351655c8b29d18bc79ef
Magnus Bendix Borregaard <magnus.borregaard@gmail.com>
2023-05-23 13:18:41 +0200

    fix plaintext content hook by just ignoring non-insert operations

--- backend/extensions/hooks/plaintext-content/index.js
+++ backend/extensions/hooks/plaintext-content/index.js
@@ -7,8 +7,8 @@ module.exports = ({ action }, ctx) => {
 
 	async function setPlaintextContent({ payload, keys, collection }, { schema, accountability }) {
 		// We are only interested in setting content_plaintext on documents, obviously
-		if (collection !== 'documents') return
-		if (!payload.content?.ops) return
+		if (collection !== 'documents') return console.warn('Not a document, skipping plaintext content update')
+		if (!payload.content?.ops) return console.warn('No content.ops, skipping plaintext content update')
 	
 		try {
 
@@ -36,8 +36,13 @@ Permission to use, copy, modify, and/or distribute this software for any purpose
 THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 function toPlaintext(delta) {
+	// Kind of hacky, but if we remove all non-insert operations (such as retain),
+	// it should have no effect on the actual plaintext content in the end,
+	// as long as there are no 'delete' operations (which there shouldn't be)
+	delta.filter(op => op.insert)
+
   return delta.reduce(function (text, op) {
-    if (!op.insert) throw new TypeError('only `insert` operations can be transformed!')
+    // if (!op.insert) throw new TypeError('only `insert` operations can be transformed!')
     if (typeof op.insert !== 'string') return text + ' '
     return text + op.insert
   }, '')
